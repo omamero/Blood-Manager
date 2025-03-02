c226aa139a1838b1b64d16a114e6b9ce9d9f23d4
omamero <83706467+omamero@users.noreply.github.com>
2025-03-02 05:54:42 +0300

    Update BloodStockManager.java

--- BloodStockManager.java
+++ BloodStockManager.java
@@ -1,3 +1,26 @@
-public class BloodStockManager {
+public class BloodStock {
+    private int hospitalID;
+    private String bloodType;
+    private int quantity;
 
+    public BloodStock(int hospitalID, String bloodType, int quantity) {
+        this.hospitalID = hospitalID;
+        this.bloodType = bloodType;
+        this.quantity = quantity;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void displayInfo() {
+        System.out.println("\nBlood Stock Details:");
+        System.out.println("Hospital ID: " + hospitalID);
+        System.out.println("Blood Type: " + bloodType);
+        System.out.println("Quantity: " + quantity + " units");
+    }
 }
