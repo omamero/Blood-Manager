6ba5c0f4fcfae7511d83917acb0112e6536f1a85
omamero <83706467+omamero@users.noreply.github.com>
2025-03-02 06:17:24 +0300

    Update Hospital.java

--- Hospital.java
+++ Hospital.java
@@ -1,3 +1,21 @@
 public class Hospital {
+    private int hospitalID;
+    private String name;
+    private String location;
+    private String contactInfo;
 
+    public Hospital(int hospitalID, String name, String location, String contactInfo) {
+        this.hospitalID = hospitalID;
+        this.name = name;
+        this.location = location;
+        this.contactInfo = contactInfo;
+    }
+
+    public void displayInfo() {
+        System.out.println("\nHospital Details:");
+        System.out.println("Hospital ID: " + hospitalID);
+        System.out.println("Name: " + name);
+        System.out.println("Location: " + location);
+        System.out.println("Contact Info: " + contactInfo);
+    }
 }
