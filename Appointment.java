e5fb9aaa2ab0be7561d132ee4204367dbe41a4b0
omamero <83706467+omamero@users.noreply.github.com>
2025-03-02 06:16:50 +0300

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -1,18 +1,14 @@
-import java.sql.Time;
 import java.util.Date;
-
 public class Appointment {
     private int appointmentID;
     private Date date;
-    private Time time;
     private String status;
     private int donorID;
     private int receiverID;
 
-    public Appointment(int appointmentID, Date date, Time time, String status, int donorID, int receiverID) {
+    public Appointment(int appointmentID, Date date, String status, int donorID, int receiverID) {
         this.appointmentID = appointmentID;
         this.date = date;
-        this.time = time;
         this.status = status;
         this.donorID = donorID;
         this.receiverID = receiverID;
@@ -22,7 +18,6 @@ public class Appointment {
         System.out.println("\nAppointment Details:");
         System.out.println("Appointment ID: " + appointmentID);
         System.out.println("Date: " + date);
-        System.out.println("Time: " + time);
         System.out.println("Status: " + status);
         System.out.println("Donor ID: " + donorID);
         System.out.println("Receiver ID: " + receiverID);
