65015934f5c6eaace27779c629e0b5e57f9f0527
omamero <83706467+omamero@users.noreply.github.com>
2025-03-02 05:53:00 +0300

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,3 +1,20 @@
-public class Admin {
+public class Admin extends User {
+    private String role;
+    private String loginCredentials;
 
+    public Admin(int id, String name, String contactInfo, String role, String loginCredentials) {
+        super(id, name, contactInfo);
+        this.role = role;
+        this.loginCredentials = loginCredentials;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("\nAdmin Details:");
+        System.out.println("ID: " + getId());
+        System.out.println("Name: " + getName());
+        System.out.println("Contact Info: " + getContactInfo());
+        System.out.println("Role: " + role);
+        System.out.println("Login Credentials: " + loginCredentials);
+    }
 }
