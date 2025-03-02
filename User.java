d12ebe706a6f76eebc28ff1ba577d69557bfdb1b
omamero <83706467+omamero@users.noreply.github.com>
2025-03-02 06:18:27 +0300

    Update User.java

--- User.java
+++ User.java
@@ -1,3 +1,25 @@
 public abstract class User {
+    private int id;
+    private String name;
+    private String contactInfo;
 
+    public User(int id, String name, String contactInfo) {
+        this.id = id;
+        this.name = name;
+        this.contactInfo = contactInfo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public abstract void displayInfo();
 }
